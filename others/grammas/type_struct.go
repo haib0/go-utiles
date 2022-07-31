770ff682cc920442309091fbeecf48a2ee415713
hdi0b <977868425@qq.com>
2022-07-31 16:47:56 +0800

    add: others/grammas/type_struct.go

---
+++ others/grammas/type_struct.go
@@ -0,0 +1,15 @@
+package grammas
+
+type FuncA func(x int) int
+
+func (f FuncA) Handle(x int) int {
+	return f(x)
+}
+
+type FuncB struct {
+	f func(x int) int
+}
+
+func (f FuncB) Handle(x int) int {
+	return f.f(x)
+}
